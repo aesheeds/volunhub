f0c0c694497823e09c03018582a78b477e1e4377
Ty Torres <tytorres55@gmail.com>
2026-06-23 18:19:37 +0000

    added anums for the various states the program can be in

--- src/parser.py
+++ src/parser.py
@@ -1,9 +1,26 @@
 import argparse
+from enum import Enum, auto
 
-parser = argparse.ArgumetnParser(description='process some files')
-parser.add_argument('filename', help='File to process')
-parser.add_argument('-o', '--output', help='Output file')
-parser.add_argument('-v', '--verbose', action='store_true', help='Verbose output')
+class CLIstate(Enum):
+    INTRO = auto()
+    CREDENTIALS = auto()
+    SEARCH = auto()
+    SAVED = auto()
+    PROFILE = auto()
+
+#todo make a better help_message
+help_message = 'This is a temporary help message to be redone later'
+
+# parser = argparse.ArgumentParser(description='process some files')
+# parser.add_argument('-h', '--help', )
+# parser.add_argument('-b', '--back', action=argparse.BooleanOptionalAction)
+# parser.add_argument('-e', '--exit', action=argparse.BooleanOptionalAction)
+# parser.add_argument('-s', '--saved', action=argparse.BooleanOptionalAction)
+# parser.add_argument('-p', '--profile', action=argparse.BooleanOptionalAction)
+# parser.add_argument('-c', '--confirm', )
+# parser.add_argument('-u', '--unconfirm', )
 
 if __name__ == "__main__":
-    print("this is the main file")
+    for state in CLIstate:
+        print(f"{state.name}: {state.value}")
+        
