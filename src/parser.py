19bbefcd462aca6f936324827a469fdc7393b81e
Ty Torres <tytorres55@gmail.com>
2026-06-23 19:07:35 +0000

    basic functioning parser UNTESTED

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,8 @@
 import argparse
 from enum import Enum, auto
 
+programname = 'Volunhub'
+
 class CLIstate(Enum):
     INTRO = auto()
     CREDENTIALS = auto()
@@ -11,16 +13,21 @@ class CLIstate(Enum):
 #todo make a better help_message
 help_message = 'This is a temporary help message to be redone later'
 
-# parser = argparse.ArgumentParser(description='process some files')
-# parser.add_argument('-h', '--help', )
-# parser.add_argument('-b', '--back', action=argparse.BooleanOptionalAction)
-# parser.add_argument('-e', '--exit', action=argparse.BooleanOptionalAction)
-# parser.add_argument('-s', '--saved', action=argparse.BooleanOptionalAction)
-# parser.add_argument('-p', '--profile', action=argparse.BooleanOptionalAction)
-# parser.add_argument('-c', '--confirm', )
-# parser.add_argument('-u', '--unconfirm', )
+
+parser = argparse.ArgumentParser(description=programname)
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-b', '--back', action='store_true', help='')
+group.add_argument('-e', '--exit', action='store_true', help='')
+group.add_argument('-s', '--saved', action='store_true', help='')
+group.add_argument('-p', '--profile', action='store_true', help='')
+group.add_argument('-c', '--confirm', action='extend', nargs='+',   help='')
+group.add_argument('-u', '--unconfirm', action='extend', nargs='+',  help='')
 
 if __name__ == "__main__":
-    for state in CLIstate:
-        print(f"{state.name}: {state.value}")
+    args = parser.parse_args()
+    print(args)
+    while not args.exit :
+        s = input("Volunhub: ")
+        args = parser.parse_args(s.split())
         
