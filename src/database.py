77ac0989bfd4dd75db1399a5ae12a19238492f14
Shellsea Nunez-Aviles <shenunavi126@gmail.com>
2026-06-25 18:38:03 +0000

    Matched PEP8 style to gemini.py, database.py, and jsearch.py

--- src/database.py
+++ src/database.py
@@ -7,8 +7,14 @@ load_dotenv()
 # load supabase variables from .env file
 supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
 
+
 # funtion to add new user into database
-def save_user(first_name, last_name, degree, major, skills, experience, location, job_type):
+
+
+def save_user(first_name, last_name,
+              degree, major, skills,
+              experience, location, job_type):
+
     data = {
         "first_name": first_name,
         "last_name": last_name,
@@ -22,10 +28,19 @@ def save_user(first_name, last_name, degree, major, skills, experience, location
     response = supabase.table("users").insert(data).execute()
     return response
 
+
 # function to get user information
+
+
 def get_user(first_name, last_name):
-    response = supabase.table("users").select("*").eq("first_name", first_name).eq("last_name", last_name).execute()
+    response = (
+                supabase.table("users")
+                .select("*")
+                .eq("first_name", first_name)
+                .eq("last_name", last_name)
+                .execute()
+    )
+
     if response.data:
         return response.data[0]
     return None
-
