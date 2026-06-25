df650a54883f879ebddc598f3365624d96c2768e
Ty Torres <tytorres55@gmail.com>
2026-06-25 17:24:10 +0000

    uncommented gemini.py

--- src/gemini.py
+++ src/gemini.py
@@ -17,34 +17,34 @@ def generate_search_query(first_name, degree, major, skills, experience, locatio
     else:
         job_label = "internship or entry-level job"
     
-    # prompt = f"""
-    #         A {degree} student named {first_name} is looking for an {job_label}.
-    #         Their info includes:
-    #         Major: {major}
-    #         Skills: {skills} 
-    #         Experience: {experience}
-    #         Location to Work: {location}. 
-    #         Generate a short job search (10 words max) query for a job search API. Include what you think is most important and will
-    #         generate the best job postings. Return only the query. No punctuation, No quotes. 
-    #         """
+    prompt = f"""
+            A {degree} student named {first_name} is looking for an {job_label}.
+            Their info includes:
+            Major: {major}
+            Skills: {skills} 
+            Experience: {experience}
+            Location to Work: {location}. 
+            Generate a short job search (10 words max) query for a job search API. Include what you think is most important and will
+            generate the best job postings. Return only the query. No punctuation, No quotes. 
+            """
     
-    # gemini_models = ["gemini-2.5-flash", "gemini-3.5-flash"]
+    gemini_models = ["gemini-2.5-flash", "gemini-3.5-flash"]
 
-    # # try different models if one fails
-    # for model in gemini_models:
+    # try different models if one fails
+    for model in gemini_models:
 
-    #     # try three times if gemini gives an error
-    #     for attempt in range(3):
-    #         try:
-    #             response = client.models.generate_content(
-    #                 model=model,
-    #                 contents=prompt
-    #             )
-    #             return response.text.strip()
+        # try three times if gemini gives an error
+        for attempt in range(3):
+            try:
+                response = client.models.generate_content(
+                    model=model,
+                    contents=prompt
+                )
+                return response.text.strip()
 
-    #         except Exception as e:
-    #             print(f"{model} attempt {attempt + 1} failed, Retrying . . .")
-    #             time.sleep(20)
+            except Exception as e:
+                print(f"{model} attempt {attempt + 1} failed, Retrying . . .")
+                time.sleep(20)
 
     print("Fall back response...")
     if skills:       
